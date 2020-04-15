5ee21b656e5d2fdd6e14a12467e850d949e6d67a
Attilio Carotenuto <carotenuto.attilio@gmail.com>
2020-04-16 01:54:54 +0200

    Pursue improvements

--- Assets/DotsSteeringLib/Scripts/Behaviours/Pursue.cs
+++ Assets/DotsSteeringLib/Scripts/Behaviours/Pursue.cs
@@ -5,17 +5,9 @@ public static class Pursue
 
     public static float3 steer(float3 agentPos, float3 targetPos, float agentMaxSpeed, float3 agentVelocity, float3 targetVelocity)
     {
-        float3 distanceVector = targetPos - agentPos;
-        float distance = math.length(distanceVector);
+        float anticipationFactor = 2f;
 
-        float3 agentMovDirection = math.normalize(agentVelocity);
-        float3 targetMovDirection = math.normalize(targetVelocity);
-
-        float3 relativeHeading = math.dot(agentMovDirection, targetMovDirection);
-        //Todo: Handle case where target and agent are facing each other
-
-        float3 anticipationMultiplier = distance / agentMaxSpeed;
-        float3 pursueTargetPos = targetPos + targetVelocity * anticipationMultiplier;
+        float3 pursueTargetPos = targetPos + targetVelocity * anticipationFactor;
 
         float3 pursueTargetDistanceVector = pursueTargetPos - agentPos;
         float3 desiredVelocity = math.normalize(pursueTargetDistanceVector) * agentMaxSpeed;
